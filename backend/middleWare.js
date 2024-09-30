68de112e3a7abd21ac0b859bc799567edfb91a17
Dayne Dellaire <daynedell@gmail.com>
2024-09-29 21:03:46 -0400

    Added proper frontend origin

--- backend/middleWare.js
+++ backend/middleWare.js
@@ -1,11 +1,10 @@
 const authorize = (req, res, next) => {
     const apiKey = req.headers['key'];
-    console.log(req.headers);
+
     if(!apiKey || apiKey !== process.env.SECRET_KEY) {
         return res.status(403).json({message: 'Forbidden'});
     }
 
-    console.log('HERE');
     next();
 }
 
