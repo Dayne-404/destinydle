f11472951d0df3de6e2a245653502e148369ab51
Dayne <daynedell@gmail.com>
2026-04-02 22:08:00 -0400

    Remove authorize

--- backend/routes/dailyWeapon.route.js
+++ backend/routes/dailyWeapon.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { getDailyWeapon } from '../controllers/dailyWeapon.controller.js';
-import authorize from '../middleware.js';
+
 
 const router = express.Router();
 
-router.get('/', authorize, getDailyWeapon );
+router.get('/', getDailyWeapon );
 
 export default router;
